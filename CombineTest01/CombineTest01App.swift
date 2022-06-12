37810ea1cca7cfee7232693b7f7cf0692aa92e80
moolnam <moolnam>
2022-06-12 16:34:39 +0900

    Initial Commit

---
+++ CombineTest01/CombineTest01App.swift
@@ -0,0 +1,17 @@
+//
+//  CombineTest01App.swift
+//  CombineTest01
+//
+//  Created by KimJongHee on 2022/06/12.
+//
+
+import SwiftUI
+
+@main
+struct CombineTest01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
